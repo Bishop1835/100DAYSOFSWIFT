2f43c21a21641c469aa359625e3392c30ec954a6
Vardhan Agarwal <72724034+Bishop1835@users.noreply.github.com>
2022-09-14 16:29:26 +0530

    Create 4_double&bollean.swift

---
+++ DAY 1/4_double&bollean.swift
@@ -0,0 +1,10 @@
+//Whenever we create a variable with a fractional number, Swift automatically gives that variable the type Double and Boolean
+
+var pi = 3.141
+
+//When creating a numeric variable, Swift decides whether to consider it an integer or a double based on whether you include a decimal point
+var intValue = 1
+var doubleValue = 1.0
+ 
+//Boolean values are either true or false
+var awesome = true
