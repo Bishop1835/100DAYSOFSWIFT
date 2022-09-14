dda2134dbc4f43681c8cf85a67b221e49ee27869
Vardhan Agarwal <72724034+Bishop1835@users.noreply.github.com>
2022-09-14 16:23:13 +0530

    Create DAY 13_multi-linestrings.swift

---
+++ DAY 13_multi-linestrings.swift
@@ -0,0 +1,17 @@
+// " " these cannot store multi line strings
+
+//the opening and closing must be on there own lines and this will show line breaks in output
+
+var mlstring= """                
+This is going
+to be
+difficult
+"""
+
+//by using "\" after each line the ouput will be in the same line
+
+var mlstring= """                
+This is going \
+to be \
+difficult \
+"""
