096cc4428cd34d16c708fd02fe64df22414bc307
Vardhan Agarwal <72724034+Bishop1835@users.noreply.github.com>
2022-09-14 16:19:43 +0530

    Create 2_strings.swift

---
+++ DAY 1/2_strings.swift
@@ -0,0 +1,6 @@
+//In Swift variables can have only 1 datatype
+
+var age=30                        //int type variable
+var population=8_000_000          //large numbers stored using " _ "
+ 
+age=20                            //this changes the value of age
