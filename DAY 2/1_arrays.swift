5cb73ec447458c67a31bfcb5bce15c9af8e5e24d
Vardhan Agarwal <72724034+Bishop1835@users.noreply.github.com>
2022-09-15 10:50:51 +0530

    Create 1_arrays.swift

---
+++ DAY 2/1_arrays.swift
@@ -0,0 +1,6 @@
+// The collections of values that are stored as a single value.
+let vardhan = "Vardhan Agarwal"
+let archit = "Archit Agarwal"
+
+let names=[vardhan, archit] //to use type annotations arrays are written in brackets: [String], [Int], [Double], and [Bool].
+name[1]                      //gives "Archit Agarwal" as index start from 0
